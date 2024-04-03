f4d3f963d75125e1218e804c3f2a2ad5b6228218
Gunjan Adya <gunjan.adya@gmail.com>
2024-04-02 23:30:47 -0400

    did an oopsie

---
+++ vga_ctrl.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/02/2024 11:12:32 PM
+-- Design Name: 
+-- Module Name: vga_ctrl - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity vga_ctrl is
+--  Port ( );
+end vga_ctrl;
+
+architecture Behavioral of vga_ctrl is
+
+begin
+
+
+end Behavioral;
